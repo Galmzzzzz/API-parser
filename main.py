67716e2c825f7136c2dc78a8635a24b39027a325
Galm <galymzhansatybaldy2@gmail.com>
2026-03-29 22:18:40 +0500

    cors setting

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from parser import parser
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # адрес фронта
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/get-data")
 def get_data():
